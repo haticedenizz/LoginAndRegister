14ae9d36d3fb3e3a2395312e0d54f4b62ac5c1a6
deniz <deniz@DESKTOP-MLQ2V2P>
2022-03-14 23:59:27 +0300

    Add project files.

---
+++ LoginAndRegister/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using LoginAndRegister.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAndRegister.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    //add role here
+                   // await _userManager.AddToRoleAsync(user, "Admin");
+                    return RedirectToAction("RegisterSuccess", "Account");
+                }
+            }
+            ModelState.AddModelError("", "Invalid Register.");
+            return View(model);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    var user = await _userManager.FindByNameAsync(model.Email);
+                    return RedirectToAction("LoginSuccess", "Account");
+
+
+                }
+            }
+            ModelState.AddModelError("", "Invalid ID or Password");
+            return View(model);
+        }
+        public ActionResult RegisterSuccess()
+        {
+            return View();
+        }
+
+        public ActionResult LoginSuccess()
+        {
+            return View();
+        }
+
+
+
+    }
+}
